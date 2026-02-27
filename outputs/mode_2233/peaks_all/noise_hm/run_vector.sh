806a3b3208ee77878c8a7250bfe673bfa8bd5a64
Vikas Jadhav <vikasjadhav@Vikass-MacBook-Air.local>
2026-02-27 10:21:24 -0500

    new plots

---
+++ outputs/mode_2233/peaks_all/noise_hm/run_vector.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export HDF5_USE_FILE_LOCKING=FALSE
+
+INJECTION_FOLDER="../../../../injections/hm_pop"
+VARIABLE_PARAMS_FILE="../../../../config_files/var_par.ini"
+DATA_FILE="../../../../config_files/data_config/data_noise.ini"
+MODEL_FILE="model.ini"
+MODE_ARRAY="22 33"
+START="0"
+END="50"
+OUTPUT_FILE="results/output_vector_${START}_${END}.hdf"
+
+python /home/vjadhavy/hmphase/hm-phase-marg/helper_functions/run_hm_model.py \
+    "$INJECTION_FOLDER" \
+    "$VARIABLE_PARAMS_FILE" \
+    "$DATA_FILE" \
+    "$MODEL_FILE" \
+    "$MODE_ARRAY" \
+    "$OUTPUT_FILE" \
+    "$START" \
+    "$END"
